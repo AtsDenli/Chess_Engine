b0bd64a63421eef76b8d6f6f9fb80844a3d00c3c
1-2-3-bossman <satadenli@gmail.com>
2023-09-22 12:30:14 +0100

    minor progress

--- main.py
+++ main.py
@@ -1,51 +1,55 @@
-import pygame
 import pieces
 import PieceTables
 import search_util
 
 def main():
-    BP1 = pieces.Pawn(0, (1,0))
-    BP2 = pieces.Pawn(0, (1,1))
-    BP3 = pieces.Pawn(0, (1,2))
-    BP4 = pieces.Pawn(0, (1,3))
-    BP5 = pieces.Pawn(0, (1,4))
-    BP6 = pieces.Pawn(0, (1,5))
-    BP7 = pieces.Pawn(0, (1,6))
-    BP8 = pieces.Pawn(0, (1,7))
-    BH1 = pieces.Horse(0, (0,1))
-    BH2 = pieces.Horse(0, (0,6))
-    BB1 = pieces.Bishop(0, (0,2))
-    BB2 = pieces.Bishop(0, (0,5))
-    BR1 = pieces.Rook(0, (0,0))
-    BR2 = pieces.Rook(0, (0,7))
-    BQ = pieces.Queen(0, (0,3))
-    BK = pieces.King(0, (0,4))
 
-    WP1 = pieces.Pawn(1, (6,0))
-    WP2 = pieces.Pawn(1, (6,1))
-    WP3 = pieces.Pawn(1, (6,2))
-    WP4 = pieces.Pawn(1, (6,3))
-    WP5 = pieces.Pawn(1, (6,4))
-    WP6 = pieces.Pawn(1, (6,5))
-    WP7 = pieces.Pawn(1, (6,6))
-    WP8 = pieces.Pawn(1, (6,7))
-    WH1 = pieces.Horse(1, (7,1))
-    WH2 = pieces.Horse(1, (7,6))
-    WB1 = pieces.Bishop(1, (7,2))
-    WB2 = pieces.Bishop(1, (7,5))
-    WR1 = pieces.Rook(1, (7,0))
-    WR2 = pieces.Rook(1, (7,7))
-    WQ = pieces.Queen(1, (7,3))
-    WK = pieces.King(1, (7,4))
+    #making the peices accessible to all functions
+    global BP1, BP2, BP3, BP4, BP5, BP6, BP7, BP8, BH1, BH2, BB1, BB2, BR1, BR2, BQ, BK
+    global WP1, WP2, WP3, WP4, WP5, WP6, WP7, WP8, WH1, WH2, WB1, WB2, WR1, WR2, WQ, WK
 
     board = [[BR1, BH1, BB1, BQ, BK, BB2, BH2, BR1],
-             [BP1, BP2, BP3, BP4, BP5, BP6, BP7, BP8], 
-             [None, None, None, None, None, None, None, None],
-             [None, None, None, None, None, None, None, None],
-             [None, None, None, None, None, None, None, None],
-             [None, None, None, None, None, None, None, None],
-             [WP1, WP2, WP3, WP4, WP5, WP6, WP7, WP8],
-             [WR1, WH1, WB1, WQ, WK, WB2, WH2, WR2]]
+            [BP1, BP2, BP3, BP4, BP5, BP6, BP7, BP8], 
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [WP1, WP2, WP3, WP4, WP5, WP6, WP7, WP8],
+            [WR1, WH1, WB1, WQ, WK, WB2, WH2, WR2]]
+
+    BP1 = pieces.Pawn(0, (1,0),board)
+    BP2 = pieces.Pawn(0, (1,1),board)
+    BP3 = pieces.Pawn(0, (1,2),board)
+    BP4 = pieces.Pawn(0, (1,3),board)
+    BP5 = pieces.Pawn(0, (1,4),board)
+    BP6 = pieces.Pawn(0, (1,5),board)
+    BP7 = pieces.Pawn(0, (1,6),board)
+    BP8 = pieces.Pawn(0, (1,7),board)
+    BH1 = pieces.Horse(0, (0,1),board)
+    BH2 = pieces.Horse(0, (0,6),board)
+    BB1 = pieces.Bishop(0, (0,2),board)
+    BB2 = pieces.Bishop(0, (0,5),board)
+    BR1 = pieces.Rook(0, (0,0),board)
+    BR2 = pieces.Rook(0, (0,7),board)
+    BQ = pieces.Queen(0, (0,3),board)
+    BK = pieces.King(0, (0,4),board)
+
+    WP1 = pieces.Pawn(1, (6,0),board)
+    WP2 = pieces.Pawn(1, (6,1),board)
+    WP3 = pieces.Pawn(1, (6,2),board)
+    WP4 = pieces.Pawn(1, (6,3),board)
+    WP5 = pieces.Pawn(1, (6,4),board)
+    WP6 = pieces.Pawn(1, (6,5),board)
+    WP7 = pieces.Pawn(1, (6,6),board)
+    WP8 = pieces.Pawn(1, (6,7),board)
+    WH1 = pieces.Horse(1, (7,1),board)
+    WH2 = pieces.Horse(1, (7,6),board)
+    WB1 = pieces.Bishop(1, (7,2),board)
+    WB2 = pieces.Bishop(1, (7,5),board)
+    WR1 = pieces.Rook(1, (7,0),board)
+    WR2 = pieces.Rook(1, (7,7),board)
+    WQ = pieces.Queen(1, (7,3),board)
+    WK = pieces.King(1, (7,4),board)
     
     gameState = 0
 
@@ -111,5 +115,10 @@ def evaluate(board):
                             WEval += PieceTables.WKing()[i][j]
                         else:
                             BEval += PieceTables.BKing()[i][j]
+
+    #Evaluating King safety
+    for i in range(2):
+        surroundings = []
+        pos = WK.pos
 
 main()
