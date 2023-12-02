bb9a6e5184675549f36c081d5869b3d2401f6293
1-2-3-bossman <satadenli@gmail.com>
2023-12-02 11:15:08 +0000

    remove short games and added the header row

--- chessGamePrep.py
+++ chessGamePrep.py
@@ -5,6 +5,8 @@ with open("chess_games.csv", "r") as org, open("chess_games_edit.csv", "w", newl
     count = 0
     for row in csv.reader(org):
         if count > 0:
-            if "bullet" not in row[0] and (int(row[6]) + int(row[7]))/2 > 1900 and '{' not in row[14]:
+            if "bullet" not in row[0] and (int(row[6]) + int(row[7]))/2 > 1900 and '{' not in row[14] and "20." in row[14]:
                 writer.writerow((row[3],row[10],row[12],row[13], row[14]))
+        if count == 0:
+            writer.writerow((row[3],row[10],row[12],row[13],row[14]))
         count+=1
