cec5977d8d7533e105caeb350c1867639c83300b
1-2-3-bossman <satadenli@gmail.com>
2023-12-03 00:25:46 +0000

    move to matrix

--- engine.py
+++ engine.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 import re
 import numpy as np
 import chess
+import pandas as pd
 
 letter2Num = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7}
 num2Letter = {0:'a', 1:'b', 2:'c', 3:'d', 4:'e', 5:'f', 6:'g', 7:'h'}
@@ -24,6 +25,31 @@ def PNG2Text(game, position):
         if move[1]:
             board.push_san(move[1])
     return board
+
+def moveMatrix(board, move):
+    piece = ''
+    for char in move:
+        if char != ' ' and char == char.upper():
+            piece = char
+    stack = Text2Stack(board)
+    k = 0
+    if piece == '':
+        k = 0
+    elif piece == 'N':
+        k = 1
+    elif piece == 'B':
+        k = 2
+    elif piece == 'R':
+        k = 3
+    elif piece == 'Q':
+        k = 4
+    elif piece == 'K':
+        k = 5
+
+    initialBoard = stack[k]
+    board.push_san(move)
+    stack2 = Text2Stack(board)
+    outputBoard = stack2[k]
 
 def Text2Stack(board):
     arraySize = (8,8)
@@ -48,4 +74,15 @@ def Text2Stack(board):
             elif board[i][j].lower() == 'k':
                 king[i][j] = 1 if board[i][j] == 'K' else -1
 
-    np.stack((pawn,knight,bishop,rook,queen,king))    
+    return np.stack((pawn,knight,bishop,rook,queen,king))    
+
+class ChessDataset(Dataset):
+    def __init__(self,games):
+        super(ChessDataset,self).__init__()
+        self.games = games
+
+    def __len__(self):
+        return 100_000
+    
+    def __getitem__(self, index):
+        pass #TODO
