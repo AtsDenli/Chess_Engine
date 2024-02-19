4c32ffdfa12bde305bd2ff0cbfd2eea178a96def
1-2-3-bossman <satadenli@gmail.com>
2024-02-19 12:19:41 +0000

    reduce number of columns and extra filtering

--- chessGamePrep.py
+++ chessGamePrep.py
@@ -5,9 +5,11 @@ def strip(png):
     pattern1 = re.compile("[ ][1234567890][.]")
     pattern2 = re.compile("[ ][1234567890][1234567890][.]")
     pattern3 = re.compile("[1234567890][.]")
+    pattern4 = re.compile("[ ][1234567890][1234567890]")
     png = re.sub(pattern2,'',png)
     png = re.sub(pattern1,'',png)
     png = re.sub(pattern3,'',png)
+    png = re.sub(pattern4,'',png)
     return png
 
 
@@ -17,7 +19,7 @@ with open("chess_games.csv", "r") as org, open("chess_games_edit.csv", "w", newl
     for row in csv.reader(org):
         if count > 0:
             if "bullet" not in row[0] and (int(row[6]) + int(row[7]))/2 > 1900 and '{' not in row[14] and "20." in row[14]:
-                writer.writerow((row[3],row[10],row[12],row[13], strip(row[14])))
+                writer.writerow((strip(row[14])))
         if count == 0:
-            writer.writerow((row[3],row[10],row[12],row[13],strip(row[14])))
+            writer.writerow((strip(row[14])))
         count+=1
