c64966266ff71925dd22fe7e1d4efb7a19547784
1-2-3-bossman <satadenli@gmail.com>
2024-02-19 20:58:06 +0000

    reduce number of columns and extra filtering

--- chessGamePrep.py
+++ chessGamePrep.py
@@ -19,7 +19,7 @@ with open("chess_games.csv", "r") as org, open("chess_games_edit.csv", "w", newl
     for row in csv.reader(org):
         if count > 0:
             if "bullet" not in row[0] and (int(row[6]) + int(row[7]))/2 > 1900 and '{' not in row[14] and "20." in row[14]:
-                writer.writerow((strip(row[14])))
+                writer.writerow((row[3], strip(row[14])))
         if count == 0:
-            writer.writerow((strip(row[14])))
+            writer.writerow((row[3],row[14]))
         count+=1
