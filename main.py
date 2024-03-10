5b0212ca7b2dbb52f4f0c37bae024eb3f0d3353e
1-2-3-bossman <satadenli@gmail.com>
2024-03-10 00:48:49 +0000

    finished pickmove function and black movecase

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from engineHelp import Text2Stack, PNG2Text, moveMatrix, GetNewBoard, StackToCha
 
 board = chess.Board()
 model = engine.NeuralNetwork()
-model.load_state_dict(torch.load("model_save.pt"))
+model.torch.load("model_save.pt")
 
 colour = input("Would you like to play black or white?")
 while colour != "black" or colour != "white":
@@ -16,7 +16,7 @@ while colour != "black" or colour != "white":
 def pickMove():
     move = ""
     while move not in list(board.legal_moves):
-        move = Text2Stack(board)
+        move = StackToChange(Text2Stack(board), model(board))
 
 def mateInOne(board):
     board = board.copy()
@@ -39,5 +39,19 @@ while not board.is_checkmate():
                 break
             except Exception:
                 print("Please input valid UCI notation")
-    tmp = mateInOne(board)
-    blackMove = tmp if tmp else pickMove()
+        tmp = mateInOne(board)
+        blackMove = tmp if tmp else pickMove()
+        board.push_uci(blackMove)
+        print(blackMove)
+    else:
+        tmp = mateInOne(board)
+        whiteMove = tmp if tmp else pickMove()
+        board.push_uci(whiteMove)
+        print(whiteMove)
+        blackMove = input("please input a move in UCI notation")
+        while True:
+            try:
+                board.push_uci(blackMove)
+                break
+            except Exception:
+                print("please input valid UCI notation")
